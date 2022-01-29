eaa76de5a0a927d8ab104cf922a165283ea90839
Dmitriy_Kantserev <dkantserev@gmail.com>
2022-01-29 19:10:45 +0300

    fix: SubTask добавлено поле Epic; fix: Manager методы работающие с subTask теперь работают без параметра Epic; refactor: классы Task, SubTask, Epic, Manager перемещены в пакет tracker;

--- src/tracker/Epic.java
+++ src/tracker/Epic.java
@@ -1,7 +1,8 @@
+package tracker;
 import java.util.ArrayList;
 
-public class Epic extends Task{ // Грандиозная задача с подзадачами.
-    ArrayList<SubTask> subTasks= new ArrayList<>();
+public class Epic extends Task { // Грандиозная задача с подзадачами.
+    ArrayList<SubTask> subTasks = new ArrayList<>();
 
     public Epic(String nameTask, String taskBody, int id, boolean taskNew, boolean inProgress, boolean done) {
         super(nameTask, taskBody, id, taskNew, inProgress, done);
@@ -9,13 +10,13 @@ public class Epic extends Task{ // Грандиозная задача с под
 
     @Override
     public String toString() {
-        return "Epic{" +
+        return "tracker.Epic{" +
                 "nameTask='" + nameTask + '\'' +
                 ", taskBody='" + taskBody + '\'' +
                 ", id=" + id +
                 ", taskNew=" + taskNew +
                 ", inProgress=" + inProgress +
                 ", done=" + done +
-                ", subTasks "+subTasks+'}';
+                ", subTasks " + subTasks + '}';
     }
 }
